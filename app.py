9a8e71fb57cebc7e8cf92a4e4d3068fe009682f7
vitthalkonaraddi <konaraddivitthal@gmail.com>
2023-07-29 05:55:20 +0100

    first calculator commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+
+obj = Flask(__name__)
+
+@obj.route('/')
+def welcome():
+    return "Welcome to the Flask"
+
+@obj.route('/cal', methods=["GET"])
+def math_operator():
+    operation = request.json["operation"]
+    number1 =  request.json["number1"]
+    number2 =  request.json["number2"]
+    
+    if operation == "add":
+        result = number1 + number2
+    elif operation == "multiply":
+        result = number1 * number2
+    elif operation == "division":
+        result = number1 / number2
+    else:
+        result = number1 - number2
+    
+    return result
+
+print(__name__)
+
+if __name__ == '__main__':
+    obj.run(debug=True, port=5000)
